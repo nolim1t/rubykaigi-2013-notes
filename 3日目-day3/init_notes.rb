182ca57762347b001e4662f9050707d2ebf4a24f
Barry <barry.teoh@perceptionz.net>
2013-06-01 11:40:36 +0900

    Fix up the init_notes.rb CLI tool

--- 3日目-day3/init_notes.rb
+++ 3日目-day3/init_notes.rb
@@ -2,7 +2,7 @@
 if ENV['filename'] != nil then
     if ARGV[0] != nil and ARGV[1] != nil then
         File.open(ENV['filename'], 'a') do |f|
-            f << "<h2>#{ARGV[0]}</h2>#{ARGV[1]}<br /><h3>Notes</h3>"
+            f << "<h2>#{ARGV[0]}</h2>\n#{ARGV[1]}\n<h3>Notes</h3>\n"
         end
     else
         puts "Please specify title and location"
