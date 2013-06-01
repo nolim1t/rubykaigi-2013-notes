36b1694ab170c8e162ee13c1f6f2e17dca96b404
Barry <barry.teoh@perceptionz.net>
2013-06-01 10:49:22 +0900

    Fix up addnotes

--- day3/AddNotes.rb
+++ day3/AddNotes.rb
@@ -1,6 +1,12 @@
 #!/usr/bin/env ruby
-# ARGV[0] 
-File.open(ENV['filename'], 'a') do |f|
-    f << "* #{ARGV[0]}\n"
+if ENV['filename'] != nil then
+    if ARGV[0] != nil then
+        File.open(ENV['filename'], 'a') do |f|
+            f << "* #{ARGV[0]}\n"
+        end
+    else
+        puts "Need to specify the message to note"
+    end
+else
+    puts "set the 'filename' environment variable"
 end
-
